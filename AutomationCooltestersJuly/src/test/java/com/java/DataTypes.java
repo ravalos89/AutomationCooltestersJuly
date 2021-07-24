92ca4b8860ebab21a14de023503fa0b578db9094
ravalos89 <automationricardo1409@gmail.com>
2021-07-24 10:57:23 -0500

    second

---
+++ AutomationCooltestersJuly/src/test/java/com/java/DataTypes.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte abc = 127;
+		short var = 30000;
+		
+		long var2 = 9223372036854775807L;
+		
+		int altura = (int) 2.54;
+		System.out.println(altura);
+		
+		
+
+	}
+
+}
