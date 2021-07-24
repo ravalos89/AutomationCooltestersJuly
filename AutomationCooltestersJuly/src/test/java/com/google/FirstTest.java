3d6ebed65b360f859e172a572a4dbc5ea7b827e2
ravalos89 <automationricardo1409@gmail.com>
2021-07-24 10:14:19 -0500

    First commit

---
+++ AutomationCooltestersJuly/src/test/java/com/google/FirstTest.java
@@ -0,0 +1,32 @@
+package com.google;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	public static WebDriver driver;
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+  
+  @Test
+  public void f() {
+	  
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chrome/chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.close();
+  }
+
+}
