687e69b4dc54fb3d70627876686585e3a21e6f56
ravalos89 <automationricardo1409@gmail.com>
2021-07-28 19:39:58 -0500

    Add new classes conditional and datatypes

---
+++ AutomationCooltestersJuly/src/test/java/com/java/ConditionalStatement.java
@@ -0,0 +1,22 @@
+package com.java;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+		
+		int velocidad = 60; // Km/h
+		int limiteVelocidad = 80;
+		
+		if(velocidad > limiteVelocidad) {
+			System.out.println("El coche va a exceso de velocidad, multa!!");
+		}else {
+			System.out.println("El coche va en la velocidad adecuada, felicidades!");
+		}
+		
+		// IF Anidado - IF ELSE IF
+		
+		boolean carretera = true;
+
+	}
+
+}
--- AutomationCooltestersJuly/src/test/java/com/java/DataTypes.java
+++ AutomationCooltestersJuly/src/test/java/com/java/DataTypes.java
@@ -8,13 +8,45 @@ public class DataTypes {
 		byte abc = 127;
 		short var = 30000;
 		
-		long var2 = 9223372036854775807L;
+//		long var2 = 9223372036854775807L;
 		
-		int altura = (int) 2.54;
+//		int altura = (int) 2.54;
+		double altura = 2.54;
 		System.out.println(altura);
 		
+		char letra = '\u00D4';
+		System.out.println(letra);
 		
-
+		String frase = "Hola";
+		String frase2 = "Mundo";
+		int frase3 = 4;
+		int frase4 = 5;
+		
+		System.out.println(frase+frase2+frase3+frase4);
+		
+		System.out.println(frase3+frase4);
+		
+		boolean isDog = true;
+		
+		int x = 3;
+		int y = 4;
+		int suma = x*y;
+		System.out.println("La suma es "+ suma);
+		
+		//UNARIO
+		int a = 3;
+		a--;
+		
+		System.out.println("El valor de a es "+ a);
+		
+		// Relacionales
+		int var1 = 3;
+		int var2 = 4;
+		boolean var3 = false;
+		
+		if(var1>=var2 || !var3) {
+			System.out.println("HERE");
+		}
 	}
 
 }
